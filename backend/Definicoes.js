25877e82c903340fb0cefce33618b27117c75e2d
AndersonPacheco <a_pacheco_o@hotmail.com>
2020-03-24 15:34:20 -0300

    Commit Back-End da Aplicação

---
+++ backend/Definicoes.js
@@ -0,0 +1,31 @@
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * GET: Buscar/listar um informação no back-end
+  * POST: Criar uma informanção no back-end
+  * PUT: Alterar uma informação no back-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parâmetros:
+   * 
+   * Query Params: Parâmetros nomeados enciados na rota após o "?" (filtros, paginação)
+   * Route Params: Parâmetros utilizados para identificar recursos
+   * Request Body: Corpo da erquisição, utilizado para criar ou alterar recursos
+   */
+
+   /**
+    * Banco de dados:
+    * 
+    * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Soft
+    * NoSQL: MongoDB, CouchDB
+    */
+
+    /**
+     * Driver: SELECT * FROM users
+     * Query Builder: table('users').select('*').where()
+     */
